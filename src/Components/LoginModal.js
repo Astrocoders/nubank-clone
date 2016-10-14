c8c8428602098187917947c8ca84da49fc118e57
Gabriel Rubens <grsabreu@gmail.com>
2016-10-13 23:29:04 -0300

    Initialize login form

--- src/Components/LoginModal.js
+++ src/Components/LoginModal.js
@@ -4,38 +4,121 @@ import {
   View,
   Text,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native'
 
 import {
   connect,
 } from 'react-redux'
 
+import t, {
+  struct,
+} from 'tcomb-form-native'
+
+// Components
+import Button from '~/Components/Button'
+
+const { Form } = t.form
+
+const LoginSchema = struct({
+  login: t.String,
+  password: t.String,
+})
+
+const { height } = Dimensions.get('window')
+
 function LoginModal({ isVisible, app }){
   return (
     <Modal
       visible={isVisible}
+      animationType="slide"
       transparent
-      onRequestClose={app.closeLoginModal}
     >
-      <View style={{flex: 1, width: null, height: null, padding: 20, backgroundColor: '#fff'}}>
-        <Text>Login modal</Text>
-        <TouchableOpacity>
-          <Text>
-            Login
-          </Text>
-        </TouchableOpacity>
+      <View style={{flex: 1, width: null, height: null}}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            backgroundColor: '#fff',
+            margin: 20,
+            borderRadius: 5,
+            padding: 10,
+          }}
+        >
+          <View
+            style={{
+              height: (height * 0.5) - 20,
+            }}
+          >
+            <View
+              style={{
+                height: 50,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  marginLeft: 10,
+                  left: 0,
+                  position: 'absolute',
+                }}
+                onPress={app.closeLoginModal}
+              >
+                <Text
+                  style={{
+                    fontSize: 40,
+                    color: '#65186E',
+                    fontWeight: '100',
+                  }}
+                >
+                  &times;
+                </Text>
+              </TouchableOpacity>
+              <Text
+                style={{
+                  fontSize: 25,
+                  color: '#65186E',
+                  fontWeight: 'bold',
+                }}
+              >
+                Login
+              </Text>
+            </View>
+
+            <View style={{paddingTop: 20}}>
+              <Form
+                type={LoginSchema}
+              />
+            </View>
+          </View>
+
+          <View
+            style={{
+              height: (height * 0.5) - 80,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+            }}
+          >
+            <Button transparent blueBorder>
+              CONFIRMAR
+            </Button>
+          </View>
+        </View>
       </View>
     </Modal>
   )
 }
 
 LoginModal.propTypes = {
-  isVisible: PropTypes.bool,
-  app: PropTypes.objectOf(PropTypes.func),
+  isVisible: PropTypes.bool.isRequired,
+  app: PropTypes.objectOf(PropTypes.func).isRequired,
 }
 
 export default connect(
-  state => ({ isVisible: state.isLoginModalActive }),
+  state => ({ isVisible: state.app.isLoginModalActive }),
   dispatch => ({
     app: {
       closeLoginModal: () => dispatch({ type: 'app/TOGGLE_LOGIN_MODAL' }),
--- src/Scenes.js
+++ src/Scenes.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { Actions, Scene } from 'react-native-router-flux'
 
 // Components
-import Login from '~/Scenes/Login'
 import FullLoader from '~/Components/FullLoader'
 import Intro from '~/Scenes/Intro'
 
@@ -19,11 +18,6 @@ export default Actions.create(
       wrapRouter={false}
       component={Intro}
       initial={true}
-    />
-    <Scene
-      key="login"
-      wrapRouter={false}
-      component={Login}
     />
   </Scene>
 );
