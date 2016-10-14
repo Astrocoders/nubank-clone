32c6857770db53a585ea9c50d004677e498e0b90
Gabriel Rubens <grsabreu@gmail.com>
2016-10-13 23:28:34 -0300

    Update Button component

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,22 +1,59 @@
-import React, { Component } from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import React, {
+  PropTypes,
+} from 'react'
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native'
 
-// Styles
-import buttonStyles from '~/Styles/button'
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 40,
+    paddingVertical: 15,
+    backgroundColor: '#fff',
+    borderRadius: 3,
+  },
+  buttonText: {
+    color: '#65186E',
+  },
+  buttonTransparent: {
+    backgroundColor: 'transparent',
+    borderColor: '#fff',
+    borderWidth: 2,
+    paddingHorizontal: 30,
+    marginHorizontal: 10,
+  },
+})
 
-export default class Button extends Component {
-  render() {
-    return (
-      <TouchableOpacity
-        style={[buttonStyles.button, this.props.style]}
-        onPress={this.props.onPress}
-      >
-        <Text
-          style={[buttonStyles.buttonText, this.props.textStyle]}
-        >
-          {this.props.text}
-        </Text>
-      </TouchableOpacity>
-    )
-  }
+export default function Button({ style, textStyle, transparent, ...props }){
+  const buttonStyles = [
+    styles.button,
+    transparent ? styles.buttonTransparent : {},
+    style,
+    props.blueBorder ? {
+      borderColor: '#65186E',
+    } : {},
+  ]
+
+  const textStyles = [
+    styles.buttonText,
+    textStyle,
+  ]
+
+  return (
+    <TouchableOpacity style={buttonStyles} {...props}>
+      <Text style={textStyles}>
+        {props.children}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+Button.propTypes = {
+  style: TouchableOpacity.propTypes.style,
+  textStyle: Text.propTypes.style,
+  transparent: PropTypes.bool,
+  blueBorder: PropTypes.bool,
+  children: Text.propTypes.children,
 }
